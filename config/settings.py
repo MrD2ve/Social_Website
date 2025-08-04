f94f7bb4b22f119b8047355bb89c0bd82a1cc7fb
Ulugbek J. <muskisgenius@gmail.com>
2025-08-05 00:33:34 +0500

    Google authentication update

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e_lr5j&x^=^=rj)!ga)9n35&&rfzzuxqzfre3#!3a7xp-0m+91
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'account.apps.AccountConfig',
+    'social_django',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -138,6 +140,31 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 AUTHENTICATION_BACKENDS = [
-'django.contrib.auth.backends.ModelBackend',
-'account.authentication.EmailAuthBackend',
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+    'social_core.backends.facebook.FacebookOAuth2',
+    'social_core.backends.twitter.TwitterOAuth',
+    'social_core.backends.google.GoogleOAuth2',
 ]
+
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+    'account.authentication.create_profile',
+]
+
+SOCIAL_AUTH_FACEBOOK_KEY = '***' # Facebook App ID
+SOCIAL_AUTH_FACEBOOK_SECRET = '***'  # Facebook Secret Key
+
+SOCIAL_AUTH_TWITTER_KEY = '***' # Twitter API Key
+SOCIAL_AUTH_TWITTER_SECRET = '***' # Twitter API Secret
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '***' # Google Client ID
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '***' # Google Client Secret
